1261f46e17dc68b56ce8339726cabcdb1aa33bf0
Alexander <alexandersviridenko98@gmail.com>
2021-07-23 01:17:59 +0300

    lesson 2

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -6,38 +6,14 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            #region Part1
-            //Console.Write("Enter your name: ");
-            //string nameOfUser = Console.ReadLine();
-
-            //Console.Write("Enter your age: ");
-            //string ageString = Console.ReadLine();
-
-            //int age = 25;
-
-            ////age = int.Parse(ageString);
-
-            ////bool success = int.TryParse(ageString, out age);
-
-            ////bool failed = success == false;
-
-            //if (int.TryParse(ageString, out age))
-            //{
-            //    Console.WriteLine("Hello, " + nameOfUser + ", " + age + "!");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("ERROR!");
-            //}
-            #endregion
-
-            while (true)
+            while (false)
             {
+                Console.Clear();
                 Console.WriteLine("Calculator");
 
                 Console.Write("First: ");
                 bool s1 = int.TryParse(Console.ReadLine(), out int number1);
-
+                
                 Console.Write("Second: ");
                 bool s2 = int.TryParse(Console.ReadLine(), out int number2);
 
@@ -83,9 +59,68 @@ namespace Lesson1
                     Console.WriteLine(errorMessage);
                 }
 
-                Console.ReadKey();
-                Console.Clear();
+                Console.WriteLine("Press ESC to exit.");
+                ConsoleKeyInfo input = Console.ReadKey();
+
+                if (input.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
             }
+
+            int number = 1;
+
+            //do
+            //{
+            //    Console.WriteLine(++number);
+            //    // number++; // number += 1; // number = number + 1;
+            //}
+            //while (number < 11);
+
+            ////for (;;)
+            {
+                //Console.WriteLine(number);
+                number -= 2;
+
+                //if (number <= -10)
+                //{
+                //    break;
+                //}
+            }
+
+            int[] intArray = new int[15];
+
+            intArray[0] = 15;
+            //intArray[25] = 0; - ERROR
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = i + 1;
+            }
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(intArray[i]);
+            }
+
+            foreach (int item in intArray)
+            {
+                //if (i % 2 == 1)
+                //{
+                //    continue;
+                //}
+
+                Console.WriteLine(item);
+            }
+
+            string b = "Bye-bye"; // var b;
+
+            Console.WriteLine(b);
         }
     }
 }
---
+++ Lesson2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+
+            Console.Write("First: ");
+            bool s1 = int.TryParse(Console.ReadLine(), out int number1);
+
+            Console.Write("Second: ");
+            bool s2 = int.TryParse(Console.ReadLine(), out int number2);
+
+            Console.Write("Operation: ");
+
+            string op = Console.ReadLine();
+            string result = "Result: ";
+            string errorMessage = "";
+
+            if (!(s1 && s2))
+            {
+                errorMessage += "Incorrect input; ";
+            }
+
+            if (op == "*")
+            {
+                result += number1 * number2;
+            }
+            else if (op == "+")
+            {
+                result += number1 + number2;
+            }
+            else if (op == "-")
+            {
+                result += number1 - number2;
+            }
+            else if (op == "/" && number2 != 0)
+            {
+                result += number1 / number2;
+            }
+            else
+            {
+                errorMessage += "Incorrect operation; ";
+            }
+
+            switch (op)
+            {
+                case "*":
+                    result += number1 * number2;
+                    break;
+                case "+":
+                    result += number1 + number2;
+                    break;
+                case "-":
+                    result += number1 - number2;
+                    break;
+                case "/" when number2 != 0:
+                    result += number1 / number2;
+                    break;
+                default:
+                    errorMessage += "Incorrect operation; ";
+                    break;
+            }
+
+            if (errorMessage.Length == 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(result);
+                Console.WriteLine(errorMessage);
+            }
+        }
+    }
+}
