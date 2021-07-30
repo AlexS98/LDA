ed57e6e208406640036f1197a3e31ea7eae7d2ba
Alexander <alexandersviridenko98@gmail.com>
2021-07-30 11:40:50 +0300

    lesson 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            int[,] field = new int[5, 5];
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int k = 0; k < field.GetLength(1); k++)
+                {
+                    field[i, k] = 0;
+                }
+            }
+
+            field[1, 1] = 7;
+
+            while (false)
+            {
+                Render(field);
+
+                int direct = GetDirection();
+
+                Move(field, direct);
+            }
+
+            //Console.WriteLine(FactorNoRec(5));
+
+            var a = new int[,] { 
+                { 5, 1, 2, 8, 99, 10 }, 
+                { 5, 1, 2, 8, 10, 5 } 
+            };
+
+            Console.WriteLine(Max(15, 100, -10, 0));
+
+            Console.WriteLine(double.IsNaN(double.NaN));
+        }
+
+        static int Factor(int value)
+        {
+            if (value <= 1)
+            {
+                return value;
+            }
+
+            return value * Factor(value - 1);
+        }
+
+        static int FactorNoRec(int value)
+        {
+            int result = 1;
+            for (int i = 1; i <= value; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static void Render(int[,] field)
+        {
+            Console.Clear();
+
+            string sep = new string('_', field.GetLength(1) * 8 + 1);
+
+            for (int i = 0; i < field.GetLength(0); i++) 
+            {
+                Console.WriteLine(sep);
+                for (int k = 0; k < field.GetLength(1); k++) 
+                {
+                    Console.Write($"|{GetCellView(field[i, k])}\t");
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine(sep);
+        }
+
+        static string GetCellView(int value)
+        {
+            string player = "☻";
+            switch (value)
+            {
+                case 7:
+                    return player;
+                default:
+                    return "";
+            }
+        }
+
+        // w - 0, a - 1, s - 2, d - 3
+
+        static int GetDirection()
+        {
+            ConsoleKey[] possibleKeys = { ConsoleKey.W, ConsoleKey.A, ConsoleKey.S, ConsoleKey.D };
+
+            do {
+                Console.Write("Input direction: ");
+                ConsoleKeyInfo input = Console.ReadKey();
+                Console.WriteLine();
+
+                for (int i = 0; i < possibleKeys.Length; i++)
+                {
+                    if (input.Key == possibleKeys[i])
+                    {
+                        return i;
+                    }
+                }
+
+                Console.WriteLine("Wrong input.");
+            }
+            while (true);
+        }
+
+        static void Move(int[,] field, int direct)
+        {
+            int x = -1, y = -1;
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int k = 0; k < field.GetLength(1); k++)
+                {
+                    if (field[i, k] == 7)
+                    {
+                        x = i;
+                        y = k;
+                        break;
+                    }
+                }
+
+                if (x >= 0)
+                {
+                    break;
+                }
+            }
+
+            field[x, y] = 0;
+
+            switch (direct)
+            {
+                case 0 when x > 0: // u
+                    x--;
+                    break;
+                case 1 when y > 0: // l
+                    y--;
+                    break;
+                case 2 when x < field.GetLength(0) - 1: // d
+                    x++;
+                    break;
+                case 3 when y < field.GetLength(1) - 1: // r
+                    y++;
+                    break;
+            }
+
+            field[x, y] = 7;
+        }
+
+        static int GetNumber(string message = "Enter number: ")
+        {
+            bool incorrectName = true;
+            int age;
+            do
+            {
+                Console.Write(message);
+                string ageString = Console.ReadLine();
+
+                if (int.TryParse(ageString, out age))
+                {
+                    incorrectName = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            while (incorrectName);
+
+            return age;
+        }
+    
+        static double Max(params double[] values)
+        {
+            if (values.Length < 1)
+            {
+                return double.NaN;
+            }
+
+            double result = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                if(result < values[i])
+                {
+                    result = values[i];
+                }
+            }
+            return result;
+        }
+    }
+}
