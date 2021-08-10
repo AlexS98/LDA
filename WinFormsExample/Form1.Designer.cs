fe4a366da14e16adca00cfc499e3813bc35d6407
Alexander <alexandersviridenko98@gmail.com>
2021-08-10 17:18:26 +0300

    win forms example

---
+++ WinFormsExample/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace WinFormsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSin = new System.Windows.Forms.Button();
+            this.brnClear = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSin
+            // 
+            this.btnSin.Location = new System.Drawing.Point(694, 409);
+            this.btnSin.Name = "btnSin";
+            this.btnSin.Size = new System.Drawing.Size(94, 29);
+            this.btnSin.TabIndex = 0;
+            this.btnSin.Text = "SIN(X)";
+            this.btnSin.UseVisualStyleBackColor = true;
+            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
+            // 
+            // brnClear
+            // 
+            this.brnClear.Location = new System.Drawing.Point(12, 12);
+            this.brnClear.Name = "brnClear";
+            this.brnClear.Size = new System.Drawing.Size(94, 29);
+            this.brnClear.TabIndex = 2;
+            this.brnClear.Text = "Clear";
+            this.brnClear.UseVisualStyleBackColor = true;
+            this.brnClear.Click += new System.EventHandler(this.brnClear_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(694, 376);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(94, 27);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            130,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.brnClear);
+            this.Controls.Add(this.btnSin);
+            this.Name = "Form1";
+            this.Text = "Playground";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSin;
+        private System.Windows.Forms.Button brnClear;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ WinFormsExample/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsExample
+{
+    public partial class Form1 : Form
+    {
+        public Graphics Graphics { get; set; }
+        public Pen DefaultPen { get; set; }
+        public Point LastClick { get; set; }
+        public bool PlotAppeared { get; set; }
+        public Brush DefaultBrush { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Graphics = this.CreateGraphics();
+
+            // set center to center of form (by default - upper left corner)
+            Graphics.TranslateTransform(Width / 2, Height / 2);
+
+            DefaultPen = new Pen(Color.Red, 3);
+
+            EventHandler onScrollChanged = delegate (object sender, EventArgs e) 
+            {
+                int lastCypher = (int)numericUpDown1.Value % 10;
+                if (lastCypher != 0)
+                {
+                    numericUpDown1.Value -= lastCypher;
+                }
+
+                if (PlotAppeared)
+                {
+                    Clear();
+                    DrawSinPlot((float)numericUpDown1.Value);
+                }
+            };
+
+            numericUpDown1.ValueChanged += onScrollChanged;
+
+            DefaultBrush = Brushes.Black;
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+
+            var mouseClick = e as MouseEventArgs;
+
+            Point coordsInOurSystem = new Point(mouseClick.Location.X - Width / 2, mouseClick.Location.Y - Height / 2);
+            if (!PlotAppeared)
+            {
+                Graphics.DrawLine(DefaultPen, LastClick, coordsInOurSystem);
+            }
+            else
+            {
+                MessageBox.Show($"Mouse click coords: {mouseClick.Location}{Environment.NewLine}Our click coords: {coordsInOurSystem}");
+            }
+
+            LastClick = coordsInOurSystem;
+        }
+
+        private void btnSin_Click(object sender, EventArgs e)
+        {
+            Clear();
+            DrawSinPlot((float)numericUpDown1.Value);
+        }        
+
+        private void Clear()
+        {
+            Graphics.Clear(DefaultBackColor);
+            PlotAppeared = false;
+            // reset to form center
+            LastClick = new Point();
+        }
+
+        private void DrawSinPlot(float scale)
+        {
+            var scaleInt = (int) scale; 
+            var blackPen = new Pen(Color.Black, 3);
+            var greenPen = new Pen(Color.Green, 5);
+            var blackPenThin = new Pen(Color.Black, 2);
+
+            Graphics.DrawLine(blackPen, 0, -(Height / 2), 0, Height / 2);
+            Graphics.DrawLine(blackPen, -(Width / 2), 0, Width / 2, 0);
+
+            PointF? prevSinPoint = null;
+
+            int size = scaleInt >= 70 ? 7 : scaleInt >= 30 ? 4 : 3;
+
+            for (int i = -(Height / 2); i < Height / 2; i += 1)
+            {
+                if (i % scaleInt == 0)
+                {
+                    Graphics.DrawLine(blackPen, size, i, -size, i);
+                    if (scaleInt >= 30 && i / scaleInt != 0)
+                    {
+                        Graphics.DrawString((i / scaleInt).ToString(), DefaultFont, DefaultBrush, new Point(7, i));
+                    }
+                }
+                else if (scaleInt >= 100 && i % (scaleInt / 10) == 0)
+                {
+                    Graphics.DrawLine(blackPenThin, -3, i, 3, i);
+                }
+            }
+
+            for (int i = -(Width / 2); i < Width / 2; i += 1)
+            {
+                if (i % scaleInt == 0)
+                {
+                    Graphics.DrawLine(blackPen, i, -size, i, size);
+                    if (scaleInt >= 30)
+                    {
+                        Graphics.DrawString((i / scaleInt).ToString(), DefaultFont, DefaultBrush, new Point(i, 7));
+                    }
+                } 
+                else if (scaleInt >= 100 && i % (scaleInt / 10) == 0)
+                {
+                    Graphics.DrawLine(blackPenThin, i, -3, i, 3);
+                }
+
+                PointF newPoint = new PointF(i, scale * MathF.Sin(-(i / scale)));
+                if (prevSinPoint == null)
+                {
+                    prevSinPoint = newPoint;
+                }
+                else
+                {
+                    if (MathF.Abs(MathF.Abs(newPoint.Y) - scale) <= 0.15 / scale)
+                    {
+                        ShowPoint(newPoint);
+                    }
+
+                    Graphics.DrawLine(greenPen, prevSinPoint.Value, newPoint);
+                    prevSinPoint = newPoint;
+                }
+            }
+
+            PlotAppeared = true;
+        }
+
+        private void ShowPoint(PointF point, Pen pen = null, int halfSize = 5)
+        {
+            Graphics.DrawEllipse(pen ?? DefaultPen, point.X - halfSize, point.Y - halfSize, halfSize * 2, halfSize * 2);
+        }
+
+        private void brnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+    }
+}
