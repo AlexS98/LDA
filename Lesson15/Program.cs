acd344c86ae9ffebcec33e5b419125f012044f3a
Alexander <alexandersviridenko98@gmail.com>
2021-09-06 23:46:38 +0300

    lesson 15 + web

---
+++ Lesson15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lesson15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserClient userClient = new UserClient();
+            var t = userClient.AddAsync("newUser");
+
+            Task act = Task.Run(() =>
+            {
+                Task.Delay(1000).GetAwaiter().GetResult();
+                Console.WriteLine("Done");
+            });
+
+            for (int i = 0; i < 100000; i++)
+            {
+                if (t.IsCompleted)
+                {
+                    break;
+                }
+
+                Console.Write(i);
+            }
+
+        }
+    }
+}
